fc2441113f800d78313a6e4cb037e941ae7471cc
Nathan Mousa <nathanmousa1@gmail.com>
2019-09-30 15:04:42 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,14 +1,5 @@
 require_relative "blackjack.rb"
 
-def runner
-  welcome #introduce the game
-  sleep 0.75
-  card_total = initial_round 
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
-end
+
 
 runner
